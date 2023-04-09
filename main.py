c8bb0b3ffcfa159144579d06383c515a59712b45
moiseefilya <Ilyamoiseev02@yandex.ru>
2023-04-10 01:47:26 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,203 @@
+import telebot
+import os
+from dotenv import load_dotenv
+from telebot import types
+
+
+load_dotenv()
+bot = telebot.TeleBot(token=os.environ.get('TOKEN'))
+
+
+application_list = []
+MERCH_LIST = ['свитшот', 'футболка']
+SIZE_LIST = ['XS', 'S', 'M', 'L', 'XL', '2XL']
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    application_list.clear()
+    mess = f'Привет, {message.from_user.first_name} \n\nВыбери нужный пункт:'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    product_name1 = types.KeyboardButton('Свитшот')
+    product_name2 = types.KeyboardButton('Футболка')
+    markup.add(product_name1, product_name2)
+    msg = bot.send_message(message.chat.id, mess, reply_markup=markup)
+    bot.register_next_step_handler(msg, choose_merch)
+
+
+def choose_merch(message):
+    try:
+        if message.text.lower() not in MERCH_LIST:
+            msg = bot.send_message(message.chat.id, 'Некорректный ввод, попробуйте снова')
+            bot.register_next_step_handler(msg, choose_merch)
+            return
+        elif message.text.lower() == 'футболка':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+            markup.add('Мальчик', 'Девочка')
+            msg = bot.send_message(message.chat.id, 'Мальчик или девочка?', reply_markup=markup)
+            bot.register_next_step_handler(msg, choose_sex)
+        else:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+            size_xs = types.KeyboardButton('XS')
+            size_s = types.KeyboardButton('S')
+            size_m = types.KeyboardButton('M')
+            size_l = types.KeyboardButton('L')
+            size_xl = types.KeyboardButton('XL')
+            size_2xl = types.KeyboardButton('2XL')
+            markup.add(size_xs, size_s, size_m, size_l, size_xl, size_2xl)
+            msg = bot.send_message(message.chat.id, 'Теперь определимся с размером:', reply_markup=markup)
+            bot.register_next_step_handler(msg, choose_size)
+        application_list.append(message.text)
+    except Exception:
+        bot.send_message(message, 'что то не так')
+
+
+def choose_sex(message):
+    sex = message.text
+    try:
+        if (sex.lower() != 'мальчик') and (sex.lower() != 'девочка'):
+            msg = bot.send_message(message.chat.id, 'Некорректный ввод, попробуйте снова')
+            bot.register_next_step_handler(msg, choose_sex)
+            return
+        application_list.append(sex)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+        size_xs = types.KeyboardButton('XS')
+        size_s = types.KeyboardButton('S')
+        size_m = types.KeyboardButton('M')
+        size_l = types.KeyboardButton('L')
+        size_xl = types.KeyboardButton('XL')
+        size_2xl = types.KeyboardButton('2XL')
+        markup.add(size_xs, size_s, size_m, size_l, size_xl, size_2xl)
+        msg = bot.send_message(message.chat.id, 'Теперь определимся с размером:', reply_markup=markup)
+        bot.register_next_step_handler(msg, choose_size)
+    except Exception:
+        bot.send_message(message, 'Что-то не так')
+
+
+def choose_size(message):
+    size = message.text.upper()
+    try:
+        if size not in SIZE_LIST:
+            msg = bot.send_message(message.chat.id, 'Некорректный ввод, попробуйте снова')
+            bot.register_next_step_handler(msg, choose_size)
+            return
+        application_list.append(size)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=5, one_time_keyboard=True)
+        one = types.KeyboardButton('1')
+        two = types.KeyboardButton('2')
+        three = types.KeyboardButton('3')
+        four = types.KeyboardButton('4')
+        five = types.KeyboardButton('5')
+        six = types.KeyboardButton('6')
+        seven = types.KeyboardButton('7')
+        eight = types.KeyboardButton('8')
+        nine = types.KeyboardButton('9')
+        other = types.KeyboardButton('...')
+        markup.add(one, two, three, four, five, six, seven, eight, nine, other)
+        msg = bot.send_message(message.chat.id, 'Выберите количество:', reply_markup=markup)
+        bot.register_next_step_handler(msg, choose_amount)
+    except Exception:
+        bot.send_message(message, 'Что-то не так')
+
+
+def choose_amount(message):
+    amount = message.text
+    try:
+        if amount == '0':
+            msg = bot.send_message(message.chat.id, 'Нельзя заказать 0')
+            bot.register_next_step_handler(msg, choose_amount)
+            return
+        elif amount == '...':
+            msg = bot.send_message(message.chat.id, 'Введите число')
+            bot.register_next_step_handler(msg, choose_amount)
+            return
+        elif not amount.isdigit():
+            msg = bot.send_message(message.chat.id, 'Введите число')
+            bot.register_next_step_handler(msg, choose_amount)
+            return
+        elif int(amount) < 0 or len(amount) > 2:
+            msg = bot.send_message(message.chat.id, 'Это слишком много...')
+            bot.register_next_step_handler(msg, choose_amount)
+            return
+        application_list.append(str(amount))
+        msg = bot.send_message(message.chat.id, 'Введите ФИО')
+        bot.register_next_step_handler(msg, process_name)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+def process_name(message):
+    name = message.text
+    result = len(name.split())
+    try:
+        if result not in [2, 3]:
+            msg = bot.send_message(message.chat.id, 'Некорректный ввод данных, попробуйте снова')
+            bot.register_next_step_handler(msg, process_name)
+            return
+        application_list.append(name)
+        msg = bot.send_message(message.chat.id, 'Введите учебную группу (либо "-"):')
+        bot.register_next_step_handler(msg, process_group)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+def process_group(message):
+    try:
+        group = message.text
+        application_list.append(str(group))
+        msg = bot.send_message(message.chat.id, 'Введите контакт (номер тел/tg/почта):')
+        bot.register_next_step_handler(msg, process_contact)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+def process_contact(message):
+    try:
+        cont = message.text
+        application_list.append(str(cont))
+        msg = bot.send_message(message.chat.id, 'Введите комментарий к заказу (либо "-")')
+        bot.register_next_step_handler(msg, process_comm)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+def process_comm(message):
+    try:
+        comm = message.text
+        application_list.append(comm)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
+        ans1 = types.KeyboardButton('Да')
+        ans2 = types.KeyboardButton('Нет')
+        markup.add(ans1, ans2)
+        msg = bot.send_message(message.chat.id, 'Вы уверены что хотите сделать заказ?', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_proof)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+def process_proof(message):
+    answer = message.text.lower()
+    try:
+        if answer == 'да' or answer == 'уверен':
+            bot.send_message(message.chat.id, 'Спасибо за заказ!\nДля заказа снова используйте /start')
+            if application_list[0].lower() == 'футболка':
+                bot.send_message(541081425,
+                                 f' <u>Продукт:</u> {application_list[0]}\n<u>Пол:</u> {application_list[1]}\n'
+                                 f'<u>Размер:</u> {application_list[2]}\n<u>Кол-во:</u> {application_list[3]}\n'
+                                 f'<u>ФИО:</u> {application_list[4]}\n<u>Группа:</u> {application_list[5]}\n'
+                                 f'<u>Контакт:</u> {application_list[6]}\n<u>Комментарий:</u> {application_list[7]}',
+                                 parse_mode='html')
+            else:
+                bot.send_message(541081425,
+                                 f' <u>Продукт:</u> {application_list[0]}\n<u>Размер:</u> {application_list[1]}\n'
+                                 f'<u>Кол-во:</u> {application_list[2]}\n<u>ФИО:</u> {application_list[3]}\n'
+                                 f'<u>Группа:</u> {application_list[4]}\n<u>Контакт:</u> {application_list[5]}\n'
+                                 f'<u>Комментарий:</u> {application_list[6]}',
+                                 parse_mode='html')
+        else:
+            bot.send_message(message.chat.id, 'Ваш заказ отменен, для заказа снова используйте /start')
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так')
+
+
+bot.polling(none_stop=True)
